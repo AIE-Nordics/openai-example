bd83531f0e27922302c8d48cd02273b774f089ad
Johan Müllern-Aspegren <johan.aspegren@gmail.com>
2023-03-06 14:02:04 +0100

    Migrated to gpt-3.5

--- routes/index.js
+++ routes/index.js
@@ -19,18 +19,26 @@ const openai = new OpenAIApi(configuration);
 
 // take care of the submit
 router.post('/', async function(req, res) {
-  let prompt = req.body.prompt
-  console.log(`prompt:`, prompt)
-  let answer = await sendRequest(prompt)
+  let messages = generateMessagesFromPrompt(req.body.prompt)
+  console.log(`prompt:`, messages)
+  let answer = await sendRequest(messages)
   console.log(`answer:`, answer)
-  res.render('index', { title: 'OpenAI Demo', answer: answer.text });
+  console.log(`answer:`, answer.message)
+  res.render('index', { title: 'OpenAI Demo', answer: answer.message.content });
 });
 
+function generateMessagesFromPrompt(_prompt){
+  return [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": _prompt},
+  ]
+}
+
 // prepare and make the call to openAI
-async function sendRequest(_prompt){
-  const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: _prompt,
+async function sendRequest(_messages){
+  const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: _messages,
       temperature: 0.6,
       max_tokens: 600,
     });
